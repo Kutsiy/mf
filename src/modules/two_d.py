a5447beeccd154f2de0e5985eff4f007f8f82311
Egor <kycuj.egor2020@gmail.com>
2026-08-19 17:44:49 +0300

    feat: first type of pendulum

--- src/modules/two_d.py
+++ src/modules/two_d.py
@@ -1,4 +1,5 @@
 import pygame
+from math import *
 
 def start_first_scene(root):
     root.destroy()
@@ -9,15 +10,31 @@ def start_first_scene(root):
 
     center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
     top_center = pygame.Vector2(screen.get_width() / 2, 0)
+    time = 0.0
+    gravity = 9.81
+    line_l = sqrt(pow((center.x - top_center.x), 2) + pow((center.y - top_center.y), 2))
+    period_T = (2 * pi) * sqrt((line_l / gravity))
+    dt = 0.01
+    w = (2 * pi) / period_T
 
+    theta_rero = radians(45)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            
         screen.fill(color="black")
-        pygame.draw.circle(surface=screen, color="white", center=center, radius=40)
+
+        time += dt
+
+        theta = theta_rero * cos((w * time))
+
+        center.x = top_center.x + line_l * sin(theta)
+        center.y = top_center.y + line_l * cos(theta)
+
+        pygame.draw.line(surface=screen, color="white", start_pos=top_center, end_pos=center, width=4)
+        pygame.draw.circle(surface=screen, color="red", center=center, radius=40,)
         pygame.display.flip()
 
     pygame.quit()
